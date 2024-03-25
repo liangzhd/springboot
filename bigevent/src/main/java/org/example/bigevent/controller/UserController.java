f63ca519760bd3a26429b8ad17070a8da72cb47d
liangzhd <liang.zhd@outlook.com>
2024-03-25 14:29:43 +0800

    big event 完成注册接口的参数校验

--- bigevent/src/main/java/org/example/bigevent/controller/UserController.java
+++ bigevent/src/main/java/org/example/bigevent/controller/UserController.java
@@ -1,30 +1,43 @@
 package org.example.bigevent.controller;
 
+import jakarta.validation.constraints.Pattern;
 import org.example.bigevent.pojo.User;
 import org.example.bigevent.pojo.Result;
 import org.example.bigevent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 注册
+     */
     @PostMapping("/register")
-    public Result register(String username, String password){
-        // 查询用户
-        User u = userService.findByUserName(username);
-        if (u == null){
-            // 没有占用
-            userService.register(username, password);
-            return Result.success();
-        }else {
-            // 占用
-            return Result.error("用户名已被占用");
-        }
+    // 使用Validated框架判断合法性
+    public Result register(@Pattern(regexp = "^\\S{6,16}$") String username, @Pattern(regexp = "^\\S{6,16}$") String password) {
+//        if (username != null && password != null &&
+//                username.length() > 5 && password.length() > 5 &&
+//                username.length() <= 16 && password.length() <= 16) {
+            // 查询
+            User u = userService.findByUserName(username);
+            if (u == null) {
+                // 注册
+                userService.register(username, password);
+                return Result.success();
+            } else {
+                return Result.error("用户名已存在");
+            }
+//        } else {
+//            return Result.error("用户名或密码格式不正确");
+//        }
     }
 }
